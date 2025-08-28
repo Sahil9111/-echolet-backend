6bb159c5c15a6646183faa33e0848d9809a9793e
Sahil soni~ <sahilsoni9111@gmail.com>
2025-08-28 22:38:46 +0530

    Add cookie-parser dependency and integrate it into the Express app

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 import dotenv from "dotenv";
+import app from "./app.js";
 // import mongoose from "mongoose";
 import connectDB from './db/index.js'
-// import express from 'express';
-// import cors from 'cors'
+
 dotenv.config({ path: '.env', quiet: true });
 
-connectDB();
-
-// const app = express()
-// app.use(cors())
-
-// const port = process.env.PORT || 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-// app.get('/api/sahil', (req, res) => {
-//   const arr = [{'name': 'sahil'}, {'city': 'indore'}, {'country': 'india'}, {'hobby': 'coding'}, {'language': 'javascript'}, {'framework': 'react'}, {'library': 'express'}, {'database': 'mongodb'}, {'version': '1.0.0'}]
-//   res.send(arr)
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port http://localhost:${port}`)
-// })
+connectDB().then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is listening on port ${process.env.PORT || 3000}`);
+  })
+}).catch((error) => {
+  console.error("Database connection error:", error);
+});
